07705ef2a5d80491d8804c3a876488478263f015
txmartin1964-code <txmartin1964@gmail.com>
2026-08-26 17:36:42 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const { runCrewMindScraper } = require('./scraperService.js');
 
 const app = express();
 const port = process.env.PORT || 3000;
